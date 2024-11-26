4c086fa3f124f003462b5bbc8edcd5cbfc59af9e
TabbyMutinda <tabitha.mutinda@ronforddigital.com>
2024-11-26 20:27:32 +0300

    Add predict tuberculosis method

--- lib/controllers/diagonosis/diagnosis_controller.dart
+++ lib/controllers/diagonosis/diagnosis_controller.dart
@@ -1,7 +1,89 @@
+import 'package:cura_vision/helpers/http_helper.dart';
+import 'package:cura_vision/models/prediction_response.dart';
 import 'package:get/get_rx/get_rx.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:dio/dio.dart' as Dio;
 
-class DiagnosisController  extends GetxController{
+class DiagnosisController extends GetxController {
+  Rx<String> selected = "".obs;
+  Rx<bool> isLoading = false.obs;
+  Rx<PredictionResponse> predictionResponse = PredictionResponse().obs;
+  Rx<String> selectedModel = "resnet34".obs;
+  Rx<String> errorMessage = "".obs;
+  Rx<bool> hasPredicted = false.obs;
+  Rx<String> imageType = "".obs;
 
-  RxString imageType = "".obs;
+  Future<bool> predictMalariaCell() async {
+    predictionResponse.value = PredictionResponse();
+    isLoading.value = true;
+    try {
+      // Validate the selected file
+      if (selected.value.isEmpty) {
+        errorMessage.value = "Please select an image.";
+        return false;
+      }
+
+      // Prepare the form data
+      Dio.FormData formData = Dio.FormData.fromMap({
+        "image": await Dio.MultipartFile.fromFile(selected.value, filename: selected.value),
+        "model": selectedModel.value,
+      });
+
+      // Send the request
+      var decodedResponse = await HttpHelper().post('/predict', formData, isMultipart: true);
+
+      if (decodedResponse is String) {
+        errorMessage.value = decodedResponse;
+        return false;
+      }
+
+      // Parse the response
+      predictionResponse.value = PredictionResponse.fromJson(decodedResponse);
+      hasPredicted.value = true;
+    
+      return true;
+    } catch (e) {
+      errorMessage.value = "An error occurred: $e";
+      return false;
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future<bool> predictTuberculosisXray() async {
+    predictionResponse.value = PredictionResponse();
+    isLoading.value = true;
+    try {
+      // Validate the selected file
+      if (selected.value.isEmpty) {
+        errorMessage.value = "Please select an image.";
+        return false;
+      }
+
+      // Prepare the form data
+      Dio.FormData formData = Dio.FormData.fromMap({
+        "image": await Dio.MultipartFile.fromFile(selected.value, filename: selected.value),
+        "model": selectedModel.value,
+      });
+
+      // Send the request
+      var decodedResponse = await HttpHelper().post('/predict/tuberculosis', formData, isMultipart: true);
+
+      if (decodedResponse is String) {
+        errorMessage.value = decodedResponse;
+        return false;
+      }
+
+      // Parse the response
+      predictionResponse.value = PredictionResponse.fromJson(decodedResponse);
+      hasPredicted.value = true;
+    
+      return true;
+    } catch (e) {
+      errorMessage.value = "An error occurred: $e";
+      return false;
+    } finally {
+      isLoading.value = false;
+    }
+  }
 }
