ab38335a1d229913f7adc6a332ae02b448189f52
TabbyMutinda <tabitha.mutinda@ronforddigital.com>
2024-11-26 20:28:06 +0300

    Add essentials

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,11 +1,13 @@
 class Constants {
   static const int appWhite = 0XFFFFFFFF;
-  static const int appPrimaryColor = 0XFF1FCC99;
+  static const int appPrimaryColor = 0XFF0FA772;
   static const int appBlack = 0XFF000000;
   static const int appTextGrey = 0XFF6C757D;
   static const int appHintTextColor = 0XFF737373;
   static const int appFillColor = 0XFFD9D9D9;
   static const int appDividerColor = 0XFFE9CDCD;
   static const int appbackgroundColor = 0XFFF5F3F3;
-  
+  static const String ENDPOINT = "https://f79a-41-90-64-84.ngrok-free.app"; 
+  static const int appDarkGrey = 0XFF1E1E1E69;
+  static const int appOrange = 0XFFF4A261;
 }
